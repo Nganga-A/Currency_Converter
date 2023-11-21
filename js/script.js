78dac2e1b4930e92c0f6e8809e7b80b1f1c6c3d7
Abed Nganga <ngangaabed@gmail.com>
2023-11-22 01:36:04 +0300

    Initialized variables and object to store currencies,inputs and conversion rates

---
+++ js/script.js
@@ -0,0 +1,5 @@
+baseCurrencyCodeSelector = document.getElementById('base-currencies')
+targetCurrencyCodeSelector = document.getElementById('target-currencies')
+currencyAmountInput = document.getElementById('amount')
+convertedAmountSection = document.getElementById('converted')
+conversion_rates = {}
