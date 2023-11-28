0b0802a5e2b2d3d1cf2bebe3eec17925cb149638
Abed Nganga <ngangaabed@gmail.com>
2023-11-29 00:10:10 +0300

    Modified updateConvertedCurrency function

--- js/script.js
+++ js/script.js
@@ -37,10 +37,11 @@ function handleBaseCurrencyUpdate() {
  * display of the converted amount.
  */
 function updateConvertedCurrency() {
+    let targetCode = document.getElementById('target-currencies').value;
     amount = document.getElementById('amount').value == '' ? 0 : parseFloat(document.getElementById('amount').value);
     currencyAmountInput = amount;
     //If the value is empty, it defaults to 0.
-    
+
     convertedAmount = amount * (conversion_rates[targetCode] ?? 0) //convert the amount
 
     convertedAmountSection.innerText = new Intl.NumberFormat("en-US", {
